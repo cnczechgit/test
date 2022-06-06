65d6ab99d1ee204d09b3d587624822060fecb555
cnczechgit <45515407+cnczechgit@users.noreply.github.com>
2022-06-06 11:58:33 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import test.Test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Test().getName());
+
+
+    }
+
+}
---
+++ src/test/Test.java
@@ -0,0 +1,27 @@
+package test;
+
+/**
+ * 
+ */
+public class Test {
+
+    /** */
+    private String name = "Test";
+
+    /**
+     * 
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }    
+    
+}
